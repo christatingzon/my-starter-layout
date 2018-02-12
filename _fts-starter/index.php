08f334b7d212cc99420aac9a4e9f94e76db041f2
Christa <christa@futuristechstudios.com>
2018-02-12 14:18:00 +0800

    added company's starter layout and file structure

---
+++ _fts-starter/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <?php include 'partials/header-styles.php';?>
+</head>
+<body>
+  <?php include 'partials/header.php';?>
+  
+  <?php include 'partials/footer.php'; ?>
+
+  <?php include 'partials/footer-scripts.php'; ?>
+</body>
+</html>
