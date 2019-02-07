a53d2e546541f354ce0bac5a45bb9a4f6987d6b9
Christa <christatingzon@gmail.com>
2019-02-07 13:24:40 +0800

    add gulp config for scripts concatenation and images optimization

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@
 
 // Load plugins
 const autoprefixer = require("autoprefixer");
+const concat = require('gulp-concat');
 const cssnano = require("cssnano");
 const del = require("del");
 const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
 const notify = require('gulp-notify');
 const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
+const uglify = require('gulp-uglify');
 
 // Define paths
 const basePaths = {
@@ -23,14 +27,42 @@ const paths = {
     dest: basePaths.dest + 'css/',
   },
   scripts: {
-    src: basePaths.src + 'vendor/**/*.js',
+    src: basePaths.src + 'scripts/**/*.js',
     dest: basePaths.dest + 'js/'
+  },
+  images: {
+    src: basePaths.src + 'fixtures/**/*',
+    dest: basePaths.dest + 'img/'
   }
 };
 
 // Clean assets
 function clean() {
   return del([basePaths.dest]);
+}
+
+// Optimize Images
+function images() {
+  return gulp
+    .src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(notify({ message: 'Image Optimization task complete' }));
 }
 
 // CSS Task
@@ -47,18 +79,29 @@ function css() {
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(notify({ message: 'Style task complete' }));}
 
+// Concat and Minify Scripts
+ function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(notify({ message: 'Script task complete' }));
+}
+
 // Watch files
 function watch() {
   gulp.watch(paths.styles.src, css);
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.scripts.src, scripts);
 }
 
 // define complex tasks
-const build = gulp.series(clean, gulp.parallel(css));
+const build = gulp.series(clean, gulp.parallel(css, images, scripts));
 
 // export tasks
-exports.css = css;
 exports.watch = watch;
-exports.build = build;
 
 // default task, called by just running `gulp`
 exports.default = build;
