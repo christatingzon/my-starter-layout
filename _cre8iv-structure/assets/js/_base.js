8d0739f0c9cf4a586ae08e7573cf0f93bb524b58
Christa <christatingzon@gmail.com>
2019-03-05 15:59:25 +0800

    add cre8iv.click starter structure

---
+++ _cre8iv-structure/assets/js/_base.js
@@ -0,0 +1,13 @@
+// BASE FRAMEWORK IMPORT
+// import all global module dependencies here:
+import $ from "jquery"
+import UIkit from "uikit"
+
+
+// GLOBALS
+// define and export global variables and functions here:
+export var answer = 42;
+
+export function sayHello(){
+    console.log('hello');
+}
